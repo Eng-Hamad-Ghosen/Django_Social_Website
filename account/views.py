ce6d3daba302cc7da6109f4fe48738bf55663d1a
Hamad <hamad.sa.ghosen@gmail.com>
2024-09-25 13:23:09 +0300

    Create register, login account

---
+++ account/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from .forms import LoginForm
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.hashers import make_password
+from.forms import UserRegistrationForm
+
+
+# Create your views here.
+
+
+def user_login(request):
+    if request.method=='POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            print('000')
+            if user:
+                print('111')
+                if user.is_active:
+                    print('222')
+                    login(request,user)
+                else:
+                    print('Not active')
+            else:
+                print('invalid Login')
+                    
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form':form})
+        
+    
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.name = new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #myApps
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
+
+STATICFILES_DIRS=[
+    BASE_DIR / "static",
+    # os.path.join(BASE_DIR,"static"),
+    '/var/www/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
